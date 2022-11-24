a637464c7d644c8af674ee8614532eea41213b01
Marchi8 <luandeoliveirasoares@gmail.com>
2022-11-24 19:53:09 -0300

    feat: post and get team routes

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from .models import Team
+from django.forms.models import model_to_dict
+import ipdb
 
-# Create your views here.
+
+class TeamView(APIView):
+    def get(self, request):
+        teams = Team.objects.all()
+        teams_list = []
+        for team in teams:
+            team_dict = model_to_dict(team)
+            teams_list.append(team_dict)
+        return Response(teams_list)
+
+    def post(self, request):
+        team = Team.objects.create(**request.data)
+        team_dict = model_to_dict(team)
+        return Response(team_dict, status.HTTP_201_CREATED)
