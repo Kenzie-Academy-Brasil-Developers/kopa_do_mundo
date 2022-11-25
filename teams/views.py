bfdc602b4a04bfd7c0d6673b1ca9bc42f98e4a21
Marchi8 <luandeoliveirasoares@gmail.com>
2022-11-25 11:30:48 -0300

    feat: patch and delete routes, all tests solved

--- teams/views.py
+++ teams/views.py
@@ -1,11 +1,10 @@
 from rest_framework.views import APIView, Request, Response, status
 from .models import Team
 from django.forms.models import model_to_dict
-import ipdb
 
 
 class TeamView(APIView):
-    def get(self, request):
+    def get(self, request: Request) -> Response:
         teams = Team.objects.all()
         teams_list = []
         for team in teams:
@@ -13,7 +12,36 @@ class TeamView(APIView):
             teams_list.append(team_dict)
         return Response(teams_list)
 
-    def post(self, request):
+    def post(self, request: Request) -> Response:
         team = Team.objects.create(**request.data)
         team_dict = model_to_dict(team)
         return Response(team_dict, status.HTTP_201_CREATED)
+
+
+class TeamViewsTest(APIView):
+    def get(self, request: Request, team_id: int) -> Response:
+        try:
+            team = Team.objects.get(id=team_id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+        team_dict = model_to_dict(team)
+        return Response(team_dict)
+
+    def patch(self, request: Request, team_id: int) -> Response:
+        try:
+            team = Team.objects.get(id=team_id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+        for key, value in request.data.items():
+            setattr(team, key, value)
+        team.save()
+        team_dict = model_to_dict(team)
+        return Response(team_dict)
+
+    def delete(self, request: Request, team_id: int) -> Response:
+        try:
+            team = Team.objects.get(id=team_id)
+        except Team.DoesNotExist:
+            return Response({"message": "Team not found"}, status.HTTP_404_NOT_FOUND)
+        team.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
